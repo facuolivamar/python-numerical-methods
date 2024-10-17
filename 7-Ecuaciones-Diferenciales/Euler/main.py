06f2431aa6b195fdf9b1e1297ac35b09203be833
facuolivamar <facundoolivam@gmail.com>
2024-10-17 08:53:45 -0300

    moved structure to class

--- 7-Ecuaciones-Diferenciales/Euler/main.py
+++ 7-Ecuaciones-Diferenciales/Euler/main.py
@@ -4,22 +4,36 @@ import numpy as np
  y1 = y0 + h * f(t0, y0)
  y1 = y0 + (f(x0)-0.5*y0)*h
 """
+class eulers():
+    def __init__(self, f, y0, t0, t_end, h):
+        self.f = f
+        self.y0 = y0
+        self.t0 = t0
+        self.t_end = t_end
+        self.h = h
+        self.t_values, self.y_values = self.eulers_method(f, y0, t0, t_end, h)
 
-def eulers_method(f, y0, t0, t_end, h):
-    n = int((t_end - t0) / h)
-    t_values = np.linspace(t0, t_end, n+1)
-    y_values = np.zeros(n+1)
-    y_values[0] = y0
-    for i in range(1, n+1):
-        y_values[i] = y_values[i-1] + h * f(t_values[i-1], y_values[i-1])
-    return t_values, y_values
+    def eulers_method(self, f, y0, t0, t_end, h):
+        n = int((t_end - t0) / h)
+        t_values = np.linspace(t0, t_end, n+1)
+        y_values = np.zeros(n+1)
+        y_values[0] = y0
+        for i in range(1, n+1):
+            y_values[i] = y_values[i-1] + h * f(t_values[i-1], y_values[i-1])
+        return t_values, y_values
+    
+    def get_values(self):
+        return self.t_values, self.y_values
+    
+    def save_to_csv(self, filename):
+        np.savetxt(filename, np.column_stack((self.t_values, self.y_values)), delimiter=",", header="t,y", comments="")
 
 # Example usage from pdf pptx
 f = lambda x, y: np.exp(0.8 * x) - 0.5 * y
 y0, t0, t_end, h = 2, 0, 4, 0.1
-t_values, y_values = eulers_method(f, y0, t0, t_end, h)
-for value in zip(t_values, y_values):
+pptx_example = eulers(f, y0, t0, t_end, h)
+for value in pptx_example.get_values():
     print(value)
 
-# save to a csv file
-np.savetxt("euler.csv", np.column_stack((t_values, y_values)), delimiter=",", header="t,y", comments="")
+pptx_example.save_to_csv("euler_pptx_example_1.csv")
+
