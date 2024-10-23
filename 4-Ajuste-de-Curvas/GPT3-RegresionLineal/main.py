8b379462289e862931714a0c2b42a5c2c64e6543
facuolivamar <facundoolivam@gmail.com>
2024-10-23 09:01:02 -0300

    Ejercicio 1.3

--- 4-Ajuste-de-Curvas/GPT3-RegresionLineal/main.py
+++ 4-Ajuste-de-Curvas/GPT3-RegresionLineal/main.py
@@ -86,3 +86,20 @@ model.display_model()
 # Plot the data and the regression line
 model.plot(title='Ejercicio 1.2')
 
+
+
+# Usage
+X = [1, 2, 3, 4, 5]
+Y = [0.5, 1.7, 3.4, 5.7, 8.4]
+
+# Create an instance of the LinearRegression class
+model = LinearRegression(X, Y)
+
+# Fit the model (calculate coefficients)
+model.fit()
+
+# Display the regression equation
+model.display_model()
+
+# Plot the data and the regression line
+model.plot(title='Ejercicio 1.3')
